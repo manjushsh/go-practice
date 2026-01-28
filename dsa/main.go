6e591ac97330910c6a646527fa79fdd005ff6358
_ <94426452+manjushsh@users.noreply.github.com>
2026-01-28 03:08:01 +0000

    add dsa folder

--- dsa/main.go
+++ dsa/main.go
@@ -1,4 +1,13 @@
 package main
 
+import (
+    "fmt"
 
-fmt.Println("Hello there! Lets Go!")
+    "github.com/manjushsh/go-practice/dsa/2026-01"
+)
+
+
+// fmt.Println("Hello there! Lets Go!")
+func main() {
+    fmt.Println(dsa.ContainerWithMostWater([]int{1,8,6,2,5,4,8,3,7}))
+}
